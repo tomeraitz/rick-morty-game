26e3cf1b6d17334b6db9511cc979c8e9180ea53c
tomeraitz <tomeraitz1@gmail.com>
2019-01-29 10:38:12 +0200

    added mobx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react'
 import logo from './logo.svg';
 import './App.css';
 
+@observer
 class App extends Component {
   render() {
     return (
