8f189183fe866db36d3c6b6ccf1a89e26360a6b5
Aviran Levi <aveloofficial@gmail.com>
2019-01-29 15:56:13 +0200

    finished shot basics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react'
 import './App.css';
+import Lasers from './components/Lasers';
 
 @observer
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Lasers />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react'
+import LaserShot from './stores/LaserShot'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const laserStore = { LaserShot }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Provider {...laserStore}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
 serviceWorker.unregister();
