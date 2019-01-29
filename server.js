c7c5c3dd79b17de1c4e58cd640e4b5ea35576806
אושרי ברוך <osher.baruch1@gmail.com>
2019-01-29 12:10:23 +0200

    backend

---
+++ server.js
@@ -0,0 +1,21 @@
+// Server Setup
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const api = require('./server/routes/api')
+
+// Mongoose setup
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/complainDB', { useNewUrlParser: true })
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/', api)
+
+app.listen(3000, function() {
+  console.log("Server is running!")
+})
---
+++ server/routes/api.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const router = express.Router()
+const bodyParser = require('body-parser')
+const moment = require('moment');
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false }))
+
+const Score = require('../model/Score')
+const Users = require('../model/Users')
+
+router.get('/bestScores', function (req, res) {
+    Users.find({}).populate({ path: 'Scores' })
+        .exec((err, results) => {
+            let users = []
+            results.forEach(u => u.scores.forEach(s => users.push({ name: u.name, score: s.score })))
+            let bestScores = users.sort((a, b) => { return b.score - a.score; });
+            res.send(bestScores);
+        })
+})
+
+router.post('/user', function (req, res) {
+    let user = new Users({
+        name: req.body.companyName,
+        email: req.body.companyDomain,
+        scores: []
+    })
+    user.save()
+    res.send("user saved")
+})
+
+router.post('/score', function (req, res) {
+    Users.findById(req.body.id).exec((err, results) => {
+        let score = new Score({
+            date: moment().format('LLLL'),
+            score: req.body.score
+        })
+        score.save()
+
+        results.scores.push(score)
+        user.save()
+        res.send("score saved")
+    });
+})
+
+module.exports = router
