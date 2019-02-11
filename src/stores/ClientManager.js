7fe122f87337356cbbe640eec6d0cb1c86e38205
Idan Saban <idan23s.g@gmail.com>
2019-02-11 15:47:45 +0200

    deploy to heroku

--- src/stores/ClientManager.js
+++ src/stores/ClientManager.js
@@ -1,6 +1,6 @@
 import { observable, action } from 'mobx'
 import io from 'socket.io-client';
-const socket = io.connect('https://rick-and-morty-space-game.herokuapp.com/')//('http://localhost:3004')
+const socket = io.connect('/')//('http://localhost:3004')
 
 class ClientManager {
     @observable gameID
