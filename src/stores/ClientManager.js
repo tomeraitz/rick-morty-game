77547529a234b0e6e7c15998158d9c74d2138c6b
tomeraitz <tomeraitz1@gmail.com>
2019-02-11 23:45:42 +0200

    uk

--- src/stores/ClientManager.js
+++ src/stores/ClientManager.js
@@ -1,6 +1,6 @@
 import { observable, action, remove } from 'mobx'
 import io from 'socket.io-client';
-const socket = io.connect('https://rick-and-morty-space-game.herokuapp.com:443/')//('http://localhost:3004/')
+const socket = io.connect('https://rick-and-morty-space-game.herokuapp.com:80/')//('http://localhost:3004/')
 
 
 class ClientManager {
