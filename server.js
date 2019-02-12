9ee53b38b01ab7d10d3d3c4930a8a5ec1469a7e5
tomeraitz <tomeraitz1@gmail.com>
2019-02-12 11:56:03 +0200

    new test for socket io

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.use('/', api)
+// app.use('/', api)
 
 
 const Game = require('./server/gameManagerLogic/GameManager')
