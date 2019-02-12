874595e7473a9d9c2f47e3e9b318b37f5b7e977d
tomeraitz <tomeraitz1@gmail.com>
2019-02-12 12:37:42 +0200

    new test for socket io2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const api = require('./server/routes/api')
 const port =   process.env.PORT || 3004
 const server = app.listen(port)//http.createServer(app);
 const io = require('socket.io')(server);
-console.log('porti!!!!!!' + port)
+
 
 module.exports = io
 
