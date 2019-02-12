657af1d57951c3a5ef3c49ae945473f07a6394b9
Idan Saban <idan23s.g@gmail.com>
2019-02-12 15:18:45 +0200

    fix build wrong path!

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(path.resolve(__dirname, 'buildgit')));
-
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
@@ -45,69 +43,70 @@ const port = process.env.PORT || 3004
 const server = app.listen(port, () => {
   console.log(`server running on ${port}`)
 });//http.createServer(app);
+
 const randomWords = require('random-words')
-// const io = require('socket.io').listen(server)
+const io = require('socket.io').listen(server)
 
-// module.exports = io
+module.exports = io
 
-// const Game = require('./server/gameManagerLogic/GameManager')
-// const Games = {}
-// // socket.io
+const Game = require('./server/gameManagerLogic/GameManager')
+const Games = {}
+// socket.io
 
-// io.on('connection', (socket) => {
+io.on('connection', (socket) => {
 
-//   console.log('connection')
+  console.log('connection')
 
-//   socket.on('newGame', () => {
-//     console.log('Someone created a new game')
-//     const gameId = `${randomWords()}-${randomWords()}-${randomWords()}`
-//     const newGame = new Game(gameId)
-//     console.log(newGame)
-//     Games[gameId] = newGame
-//     Games[gameId].joinGame(socket.id)
-//     socket.join(`${gameId}`)
-//     const info = { gameId, playerId: Games[gameId].spaceShips.length - 1 }
-//     socket.emit('joinedGame', info)
-//   })
+  socket.on('newGame', () => {
+    console.log('Someone created a new game')
+    const gameId = `${randomWords()}-${randomWords()}-${randomWords()}`
+    const newGame = new Game(gameId)
+    console.log(newGame)
+    Games[gameId] = newGame
+    Games[gameId].joinGame(socket.id)
+    socket.join(`${gameId}`)
+    const info = { gameId, playerId: Games[gameId].spaceShips.length - 1 }
+    socket.emit('joinedGame', info)
+  })
 
-//   socket.on('joinGame', (gameId) => {
-//     Games[gameId].joinGame(socket)
-//     socket.join(gameId)
-//     socket.emit('joinedGame', { gameId, playerId: Games[gameId].spaceShips.length - 1 })
-//     io.in(`${gameId}`).emit('getReady');
-//   })
+  socket.on('joinGame', (gameId) => {
+    Games[gameId].joinGame(socket)
+    socket.join(gameId)
+    socket.emit('joinedGame', { gameId, playerId: Games[gameId].spaceShips.length - 1 })
+    io.in(`${gameId}`).emit('getReady');
+  })
 
-//   socket.on('startGame', (gameId) => {
-//     Games[gameId].start()
-//   })
+  socket.on('startGame', (gameId) => {
+    Games[gameId].start()
+  })
 
-//   socket.on('pauseGame', (gameId) => {
-//     Games[gameId].pauseGame()
-//   })
+  socket.on('pauseGame', (gameId) => {
+    Games[gameId].pauseGame()
+  })
 
-//   socket.on('continueGame', (gameId) => {
-//     Games[gameId].continuePlaying()
-//   })
+  socket.on('continueGame', (gameId) => {
+    Games[gameId].continuePlaying()
+  })
 
-//   socket.on('finishExplosion', (gameId) => {
-//     Games[gameId].finishExplosion()
-//   })
+  socket.on('finishExplosion', (gameId) => {
+    Games[gameId].finishExplosion()
+  })
 
-//   socket.on('move', (gameId, playerIndex, direction) => {
-//     Games[gameId] ? Games[gameId].move(playerIndex, direction) : null
-//   })
+  socket.on('move', (gameId, playerIndex, direction) => {
+    Games[gameId] ? Games[gameId].move(playerIndex, direction) : null
+  })
 
-//   socket.on('shoot', (gameID, playerIndex) => {
-//     Games[gameID].shoot(playerIndex)
-//   })
-
-
-//   socket.on('deleteGame', (gameID) => {
-//     delete Games[gameID]
-//     socket.emit('deleteThisGame', null)
-//   })
+  socket.on('shoot', (gameID, playerIndex) => {
+    Games[gameID].shoot(playerIndex)
+  })
 
 
-// })
+  socket.on('deleteGame', (gameID) => {
+    delete Games[gameID]
+    socket.emit('deleteThisGame', null)
+  })
+
+
+})
 
 
